3e073b63ccb3d1d3d121d2350b6aa267d4313407
megafisto <phrsilva.adv@gmail.com>
2025-02-03 01:42:30 -0300

    correção no index.js do knex

--- src/baseDeDados/knex/index.js
+++ src/baseDeDados/knex/index.js
@@ -3,14 +3,10 @@ const knex = require("knex");
 
 const conexao = knex(config.development);
 
-// Executa as migrações ao iniciar o servidor
-conexao.migrate.latest()
-    .then(() => {
-        console.log("📦 Banco de dados pronto! Migrações aplicadas.");
-    })
-    .catch(error => {
-        console.error("Erro ao executar migrações:", error);
-    });
+
+
+
+
 
 
 module.exports = conexao;
