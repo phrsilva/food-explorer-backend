ab7e520a6b5a4cd808900aa39f2809a2af3cd575
megafisto <phrsilva.adv@gmail.com>
2025-02-04 19:59:46 -0300

    adicionando rota do render no cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const rotas = require("./rotas");
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://food-explorer-backend-uscw.onrender.com/" ],
   credentials: true
 }));
 
