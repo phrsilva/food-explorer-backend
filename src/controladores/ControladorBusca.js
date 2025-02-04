7bbfb0a65dcaa6f3548302f07649c3cd4da0c993
megafisto <phrsilva.adv@gmail.com>
2025-02-03 22:17:58 -0300

    buscando pratos por ingredientes

--- src/controladores/ControladorBusca.js
+++ src/controladores/ControladorBusca.js
@@ -12,7 +12,24 @@ class ControladorBusca {
                 .where("nome", "like", `%${termo}%`)
                 .select("id", "nome");
 
-            res.json(pratosPorNome);
+            const pratosPorIngrediente = await knex("ingredientes")
+                .where("nome", "like", `%${termo}%`)
+                .select("prato_id")
+                .distinct();
+
+            const idsPratosPorIngrediente = pratosPorIngrediente.map(item => item.prato_id);
+
+            const pratosPorIngredientes = await knex("pratos")
+                .whereIn("id", idsPratosPorIngrediente)
+                .select("id", "nome");
+
+            const todosPratos = [...pratosPorNome, ...pratosPorIngredientes];
+            const pratosUnicos = Array.from(new Map(todosPratos.map(prato => [prato.id, prato])).values());
+
+
+
+
+            res.json(pratosUnicos);
         } catch (error) {
             console.error("Erro na busca:", error);
             res.status(500).json({ message: "Erro ao buscar pratos" });
