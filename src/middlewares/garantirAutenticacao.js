323608a07d048b2845126bdcef5b1972dac12e7f
megafisto <phrsilva.adv@gmail.com>
2025-02-03 21:50:47 -0300

    removendo logs do garantir Autenticacao

--- src/middlewares/garantirAutenticacao.js
+++ src/middlewares/garantirAutenticacao.js
@@ -4,18 +4,15 @@ const configuracaoDeAutenticacao = require("../configs/aut");
 
 function garantirAutenticacao(req, res, next) {
     const cabecalhoAutenticacao = req.headers.authorization;
-    console.log('teste1 ----->',cabecalhoAutenticacao); // Verifique o cabecalho de autenticacao
 
     
     if (!cabecalhoAutenticacao) {
         throw new ErroNoApp("Token não informado", 401);
     }
 
-    console.log('teste2 ----->',cabecalhoAutenticacao); // Verifique o cabecalho de autenticacao
 
 
     const [, token] = cabecalhoAutenticacao.split(" ");
-    console.log('teste3 ======',token); // Verifique o cabecalho de autenticacao
 
     try {
         const { sub: usuario_id } = verify(token, configuracaoDeAutenticacao.jwt.secret);
